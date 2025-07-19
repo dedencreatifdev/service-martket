cefdc221ae7cbec1b6542c87ffa0a610da985475
dedencreatifdeveloper <dedencreatifdeveloper@gmail.com>
2025-07-19 07:54:20 +0000

    .

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Livewire\Admin\Dashboard\DashboardIndex;
 use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'second'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/', DashboardIndex::class)->name('dashboard.index');
 });
 
